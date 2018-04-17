30af8ee59ca1ba558fcdf2b42b6f3ff13d015052
ngviethoang <ngviethoang0212@gmail.com>
2018-04-17 14:45:18 +0000

    .

--- drive.sh
+++ drive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 fileid="1f5XbJOtYkgEh1ZHtR_inM3Pc6YgjLF9_"
-filename="ds.zip"
+filename="datasets.zip"
+
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
